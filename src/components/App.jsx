c352d7ff82378de2ca0f95721d12da8914471b3c
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-05-27 20:12:21 +0300

    some tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+import React from "react";
+import Profile from "./profile/Profile";
+import user from "./profile/user.json";
+import Statistics from "./statistics/Statistics";
+import data from "./statistics/data.json";
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    <div className="App">
+  <Profile
+        username={user.username}
+        location={user.location}
+        tag={user.tag}
+        avatar={user.avatar}
+        stats={user.stats}
+      /> 
+      <Statistics title="Upload stats" stats={data}/> 
+      <Statistics stats={data} />
+</div>
+  )
 };
---
+++ src/components/statistics/Statistics.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const Statistics = ({title, stats}) => {
+    return <section className="statistics">
+    {title && <h2 className="title">{title}</h2>}
+        <ul className="stat-list">
+            {stats.map(({id, label, percentage}) => (
+                <li
+                key={id}
+                className="item"
+                style={{background: getRandomHexColor()}}>
+                    <span className="label">{label}</span>
+                    <span className="percentage">{percentage}</span>
+                </li>
+            ))}
+        </ul>
+    </section>
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+      })
+    ),
+  };
+
+export default Statistics;
+
+// <li className="item">
+//             <span className="label">{stats.label}</span>
+//             <span className="percentage">{stats.percentage}%</span>
+//         </li>
+//         <li className="item">
+//             <span className="label">{stats.label}</span>
+//             <span className="percentage">{stats.percentage}%</span>
+//         </li>
+//         <li className="item">
+//             <span className="label">{stats.label}</span>
+//             <span className="percentage">{stats.percentage}%</span>
+//         </li>
+//         <li className="item">
+//             <span className="label">{stats.label}</span>
+//             <span className="percentage">{stats.percentage}%</span>
+//         </li>
