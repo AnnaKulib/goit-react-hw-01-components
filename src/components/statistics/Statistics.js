f461a5210fa7f0312457935288553f348bcd8d02
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-05-30 23:13:18 +0300

    adds styles

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,37 +1,41 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
-
-const Statistics = ({title, stats}) => {
-    return <section className="statistics">
-    {title && <h2 className="title">{title}</h2>}
-        <ul className="stat-list">
-            {stats.map(({id, label, percentage}) => (
-                <li
-                key={id}
-                className="item"
-                style={{background: getRandomHexColor()}}>
-                    <span className="label">{label}</span>
-                    <span className="percentage">{percentage}</span>
-                </li>
-            ))}
-        </ul>
-    </section>
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.statTitle}>{title}</h2>}
+      <ul className={s.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            key={id}
+            className={s.statListItem}
+            style={{ background: getRandomHexColor() }}
+          >
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
 Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
-        percentage: PropTypes.number.isRequired,
-      })
-    ),
-  };
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default Statistics;
 
